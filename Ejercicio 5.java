04580aca94d2e95118877839cea77dff35c6ab74
RAESONKYUNG <139029008+RAESONKYUNG@users.noreply.github.com>
2023-09-17 22:46:27 -0500

    Add files via upload

---
+++ Ejercicio 5.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // Horas trabajadas por el empleado
+        int horasTrabajadas = 40; // Puedes cambiar este valor
+
+        // Tarifa por hora
+        double tarifaPorHora = 12.0;
+
+        // Calcular el salario semanal
+        double salarioSemanal = horasTrabajadas * tarifaPorHora;
+
+        // Mostrar el resultado
+        System.out.println("El salario semanal del empleado es de " + salarioSemanal + " euros.");
+    }
+}
+
