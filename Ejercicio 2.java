9bbd6e78f4c614c54bd0361412b09fdffe6e5040
RAESONKYUNG <139029008+RAESONKYUNG@users.noreply.github.com>
2023-09-17 22:40:06 -0500

    Add files via upload

---
+++ Ejercicio 2.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declarar y asignar valores a las variables
+        String nombre = "Juan Pérez";
+        String dirección = "Calle Principal, 123";
+        String teléfono = "555-123-4567";
+
+        // Mostrar los valores de las variables
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Dirección: " + dirección);
+        System.out.println("Teléfono: " + teléfono);
+    }
+}
